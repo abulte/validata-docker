64ec9f59aa171ca079d301d65f10d4d312c5400f
Alexandre Bulté <alexandre@bulte.net>
2019-02-21 10:08:17 +0100

    Add build script

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+docker-compose --build --no-cache
+
+docker tag validata-docker_api abulte/validata-api
+docker tag validata-docker_ui abulte/validata-ui
+
+docker push abulte/validata-api
+docker push abulte/validata-ui
