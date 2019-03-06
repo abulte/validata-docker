dc9fdff13685c6fbcdd7427f09c27800f65af47f
Alexandre Bulté <alexandre@bulte.net>
2019-03-06 14:46:10 +0100

    fix build script

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker-compose build --no-cache
+docker-compose -f docker-compose.dev.yml build --no-cache
 
 docker tag validata-docker_api abulte/validata-api
 docker tag validata-docker_ui abulte/validata-ui
